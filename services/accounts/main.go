5fe961075669de2cc068fff05f0d4e62919fa4fd
Wanderson Camargo <wandersonwhcr@gmail.com>
2020-12-15 20:37:21 -0300

    Utiliza o Mux para executar o serviço

--- services/accounts/main.go
+++ services/accounts/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+    "github.com/gorilla/mux"
     "net/http"
     "os"
 )
 
+func Home(writer http.ResponseWriter, request *http.Request) {
+    writer.WriteHeader(http.StatusOK)
+    writer.Write([]byte("Accounts"))
+}
+
 func main() {
-    http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+    router := mux.NewRouter()
+
+    router.HandleFunc("/", Home).Methods("GET")
+
+    http.ListenAndServe(":" + os.Getenv("PORT"), router)
 }
