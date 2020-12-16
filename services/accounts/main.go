ad0031c1d85170a8aa881c15bab607be1813c10b
Wanderson Camargo <wandersonwhcr@gmail.com>
2020-12-15 22:38:17 -0300

    Inicializa conexão com o banco de dados

--- services/accounts/main.go
+++ services/accounts/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
     "accounts/handlers"
+    "context"
     "github.com/gorilla/mux"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
     "net/http"
     "os"
+    "time"
     middlewares "github.com/gorilla/handlers"
 )
 
 func main() {
+    client, _ := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+    ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+    client.Connect(ctx)
+
     router := mux.NewRouter()
 
     router.HandleFunc("/", handlers.Home).Methods("GET")
