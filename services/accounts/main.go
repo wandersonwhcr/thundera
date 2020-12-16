c127e5159ab4754d6cd37baf424b837d6fdde9f0
Wanderson Camargo <wandersonwhcr@gmail.com>
2020-12-15 21:17:47 -0300

    Apresenta JSON na rota principal

--- services/accounts/main.go
+++ services/accounts/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "github.com/gorilla/handlers"
     "github.com/gorilla/mux"
     "net/http"
@@ -8,8 +9,13 @@ import (
 )
 
 func Home(writer http.ResponseWriter, request *http.Request) {
+    home := map[string]string{
+        "name": "accounts",
+    }
+
+    writer.Header().Set("Content-Type", "application/json")
     writer.WriteHeader(http.StatusOK)
-    writer.Write([]byte("Accounts"))
+    json.NewEncoder(writer).Encode(home)
 }
 
 func main() {
